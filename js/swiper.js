41d2f5f3964dc0d766e9c53f6dd57de6e5336c3f
Yurii Kavuza <kavuzaiurii@gmail.com>
2023-07-18 13:45:18 +0300

    change header icon size

---
+++ js/swiper.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.customer-slider', {
+    loop: true,
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+    el: '.swiper-pagination',
+    },
+    autoplay:{delay: 1},
+
+    speed: 2000,
+    
+    
+    slidesPerView: 3,    
+});
+
+// const swiper = new Swiper('.swiper');
+
+// , {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// }
