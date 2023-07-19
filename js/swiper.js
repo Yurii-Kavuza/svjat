67a2977e3e49f69343d7fbc589624e122c09591b
Yurii Kavuza <kavuzaiurii@gmail.com>
2023-07-19 17:20:55 +0300

    add customer slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,42 @@
-new Swiper('.customer-slider', {
+const refs = {
+customerNextBtn: document.querySelector('.swiper-button-next--customer'),
+customerPrevBtn: document.querySelector('.swiper-button-prev--customer'),
+};
+
+const swiper = new Swiper('.customer-slider', {
     loop: true,
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
+    // navigation: {
+    // nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
+    // },
     pagination: {
     el: '.swiper-pagination',
     },
-    // autoplay:{delay: 0},
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
 
-    speed: 3000,
-    
-    
-    slidesPerView: 3,    
+    speed: 2000,
+    freeMode: true,
+        
+    breakpoints: {
+        312: {
+            slidesPerView: 3,
+        },
+        480: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+        1400: {
+            slidesPerView: 6,
+        }
+    }
 });
+
+console.log(refs.customerNextBtn);
+
+refs.customerNextBtn.addEventListener('click', (e)=>swiper.slideNext());
+refs.customerPrevBtn.addEventListener('click', (e)=>swiper.slidePrev());
